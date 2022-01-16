9b5c6f78b12851ed63e9126bbb8752854addfe94
Quangdao Nguyen <me@quangdao.com>
2022-01-16 00:16:37 -0600

    Featured card template

--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -28,6 +28,9 @@
             }
           }
         }
+				featuredCardSettings {
+					featuredCard
+				}
       }
     }
   `;
--- src/templates/featured-cards.svelte
+++ src/templates/featured-cards.svelte
@@ -1,8 +1,29 @@
-<script>
+<script lang="ts">
 	import Post from './post.svelte';
-  export let post;
+	export let post;
+	const featuredCardId = post.featuredCardSettings.featuredCard;
+
+	const imageUrl = (id: string) => `https://api.scryfall.com/cards/${id}?format=image&version=png`;
 </script>
 
 <Post {post}>
-  <div>Card content: {@html post.content}</div>
+	<div class="post">
+		<img class="featured-card" src={imageUrl(featuredCardId)} alt={post.title} width="300" />
+		{@html post.content}
+	</div>
 </Post>
+
+<style>
+	.post::after {
+		content: '';
+    display: block;
+		clear: both;
+	}
+  
+	.featured-card {
+		display: block;
+		max-width: 300px;
+		float: left;
+		margin-right: 1em;
+	}
+</style>
