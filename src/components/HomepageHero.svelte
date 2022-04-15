996f310e4923e6ace2081d4a07ea31968c6b8861
Quangdao Nguyen <me@quangdao.com>
2022-04-14 22:46:42 -0500

    Hero fade out

--- src/components/HomepageHero.svelte
+++ src/components/HomepageHero.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	export let description;
 	export let card;
 
@@ -6,11 +6,16 @@
 		'https://c1.scryfall.com/file/scryfall-cards/art_crop/front/2/2/225a0644-184e-4216-ac53-09a8a33538a2.jpg?1645341008';
 	import ParallaxHero from './ParallaxHero.svelte';
 	import FeaturedCard from './FeaturedCard.svelte';
+
+	let scroll: number;
+	$: bgOpacity = 0.5 + scroll / 1000;
 </script>
+
+<svelte:window bind:scrollY={scroll} />
 
 <div class="homepage-hero">
 	<ParallaxHero src={img}>
-		<div slot="content" class="content">
+		<div slot="content" class="content" style="--bg-opacity: {bgOpacity}">
 			<div class="container">
 				<div class="intro">
 					<h2>{description}</h2>
@@ -38,11 +43,12 @@
 		}
 	}
 	.content {
-		background: rgba(#000, 0.5);
+		background: rgba(#000, var(--bg-opacity));
 		height: 100%;
 	}
 	.container {
 		text-align: center;
+		margin-top: 0;
 		margin-bottom: 0;
 		:global(.featured-card) {
 			max-width: 300px;
