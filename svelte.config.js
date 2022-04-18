ab28d4ac82b96864b9cdacb4780376144a544802
Quangdao Nguyen <me@quangdao.com>
2022-04-18 12:28:14 -0500

    Remove explicit Netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import preprocess from 'svelte-preprocess';
 import { resolve } from 'path';
-import netlify from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +7,6 @@ const config = {
 		scss: { prependData: `@import 'src/styles/globals.scss';` }
 	}),
 	kit: {
-		adapter: netlify(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		vite: {
 			resolve: {
